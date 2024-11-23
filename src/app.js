25bcaccf3ddfdc3d125ac3a4b37eecb8be1c8e1c
nghiatk54 <nghiatk54@gmail.com>
2024-11-23 19:24:01 +0700

    install application

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const app = express();
+const morgan = require("morgan");
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init database
+
+// init routes
+app.get("/", (req, res, next) => {
+    const strCompress = "Hello Fantipjs!";
+    return res.status(200).json({
+        message: "Welcom Fantipjs!",
+        metadata: strCompress.repeat(1000),
+  });
+});
+
+// handle error
+
+module.exports = app;
