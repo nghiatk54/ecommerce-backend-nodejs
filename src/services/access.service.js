b298e0f89d7c73742c4cbd69d517811d6a11ced7
nghiatk54 <nghiatk54@gmail.com>
2024-12-02 05:46:08 +0700

    Error and success response Handler

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -6,6 +6,7 @@ const crypto = require('node:crypto')
 const KeyTokenService = require("./keyToken.service")
 const { createTokenPair } = require("../auth/authUtils")
 const { getInfoData } = require("../utils")
+const { BadRequestError, ConflictRequestError } = require("../core/error.response")
 const RoleShop = {
     SHOP: 'SHOP',
     WRITER: 'WRITER',
@@ -14,65 +15,51 @@ const RoleShop = {
 }
 
 class AccessService {
-    static signUp = async ({name, email, password}) => {
-        try {
-            // step 1: check email exists?
-            const holderShop = await shopModel.findOne({email}).lean()
-            if (holderShop) {
-                return {
-                    code: 'xxxx',
-                    message: 'Shop already registered!'
-                }
-            }
+    static signUp = async ({ name, email, password }) => {
+        // step 1: check email exists?
+        const holderShop = await shopModel.findOne({ email }).lean()
+        if (holderShop) {
+            throw new BadRequestError('Error: Shop already registered!')
+        }
 
-            // step 2: create new shop
-            const passwordHash = await bcrypt.hash(password, 10)
-            const newShop = await shopModel.create({
-                name, email, password: passwordHash, roles: [RoleShop.SHOP]
+        // step 2: create new shop
+        const passwordHash = await bcrypt.hash(password, 10)
+        const newShop = await shopModel.create({
+            name, email, password: passwordHash, roles: [RoleShop.SHOP]
+        })
+
+        if (newShop) {
+            // create public and private key
+            const privateKey = crypto.randomBytes(64).toString('hex')
+            const publicKey = crypto.randomBytes(64).toString('hex')
+
+            console.log({ privateKey, publicKey }) // save collection KeyStore
+
+            const keyStore = await KeyTokenService.createKeyToken({
+                userId: newShop._id,
+                publicKey,
+                privateKey
             })
 
-            if (newShop) {
-                // create public and private key
-                const privateKey = crypto.randomBytes(64).toString('hex')
-                const publicKey = crypto.randomBytes(64).toString('hex')
+            if (!keyStore) {
+                throw new BadRequestError('KeyStore error')
+            }
 
-                console.log({ privateKey, publicKey }) // save collection KeyStore
-
-                const keyStore = await KeyTokenService.createKeyToken({
-                    userId: newShop._id,
-                    publicKey,
-                    privateKey
-                })
-
-                if(!keyStore) {
-                    return {
-                        code: 'xxxx',
-                        message: 'KeyStore error'
-                    }
-                }
-
-                // Created token pair
-                const tokens = await createTokenPair({userId: newShop._id, email}, publicKey, privateKey)
-                console.log(`Created Token Success::`, tokens)
-                return {
-                    code: 201,
-                    metadata: {
-                        shop: getInfoData({fileds: ['_id', 'name', 'email'], object: newShop}),
-                        tokens
-                    }
+            // Created token pair
+            const tokens = await createTokenPair({ userId: newShop._id, email }, publicKey, privateKey)
+            console.log(`Created Token Success::`, tokens)
+            return {
+                code: 201,
+                metadata: {
+                    shop: getInfoData({ fileds: ['_id', 'name', 'email'], object: newShop }),
+                    tokens
                 }
             }
+        }
 
-            return {
-                code: 200,
-                metadata: null
-            }
-        } catch (error) {
-            return {
-                code: 'xxx',
-                message: error.message,
-                status: 'error'
-            }
+        return {
+            code: 200,
+            metadata: null
         }
     }
 }
